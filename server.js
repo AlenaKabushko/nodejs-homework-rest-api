19d05faf72bcedb67fa995765e33eaef0cd249eb
Alena <100042777+AlenaKabushko@users.noreply.github.com>
2023-01-12 18:58:02 +0200

    add connect to db

--- server.js
+++ server.js
@@ -1,5 +1,24 @@
-const app = require('./app')
+const app = require('./app');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+mongoose.set('strictQuery', false);
+dotenv.config();
+
+const { HOST_URI } = process.env;
+
+async function main() {
+  try {
+    await mongoose.connect(HOST_URI)
+    console.log("Database connection successful")
+
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000")
+    })
+  } catch (error) {
+    console.log("Error connect MongoDB", error.message)
+    process.exit(1)
+  }
+}
+
+main();
--- utils/validSchema.js
+++ utils/validSchema.js
@@ -3,14 +3,14 @@ const Joi = require("joi");
 const validSchemaPost = Joi.object({
   name: Joi.string().min(1).max(40).required(),
   email: Joi.string().min(5).email().required(),
-  phone: Joi.number().min(6).integer().required(),
+  phone: Joi.number().min(1).integer().required(),
 });
 
 
 const validSchemaPut = Joi.object({
   name: Joi.string().min(1).max(40),
   email: Joi.string().min(5).email(),
-  phone: Joi.number().min(6).integer(),
+  phone: Joi.number().min(1).integer(),
 }).min(1);
 
 module.exports = {
